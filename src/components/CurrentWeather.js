34a1f7c71f2eae14e8c6a44f13048bcfd068b32b
Philip Harrell <alightedlamp@gmail.com>
2017-06-26 21:25:17 -0500

    temp display

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
-import { HttpClient } from '../helpers';
+import axios from 'axios';
 
 class CurrentWeather extends Component {
-    getWeather() {
-        console.log('here');
-        const geoNotSupported = true;
+    constructor() {
+        super();
+
+        this.getWeather = this.getWeather.bind(this);
+
+        this.state = {
+            temp: [],
+            weather: []
+        };
+    }
+    getWeather(position) {
         var coords = [];
+        coords[0] = position.coords.latitude;
+        coords[1] = position.coords.longitude;
+
+        const appId = "77f24c065514660d48a68b8d6a138ce2";
+        const cb = "&callback=JSON_CALLBACK";
+        const base_url = "http://api.openweathermap.org/data/2.5/weather";
+        axios.get(base_url, {
+            params: {
+                lat: coords[0],
+                lon: coords[1],
+                units: "imperial",
+                APPID: appId
+            }
+        }).then(response => {
+            const temp = response.data.main;
+            const weather = response.data.weather;
+
+            this.setState({ weather });
+            this.setState({ temp });
+
+            console.log(weather);
+            console.log(temp);
+        });
+    }
+    componentDidMount() {
+        const geoNotSupported = true;
 
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                coords[0] = position.coords.latitude;
-                coords[1] = position.coords.longitude;
-
-                const client = new HttpClient();
-                const base_url = "https://api.darksky.net/forecast/bf8b595964753a2d307f80ffc916bea9/"
-                client.get(base_url + coords[0] + "," + coords[1], function(response) {
-                    console.log("response is: " + response);
-                });
-        });
+            navigator.geolocation.getCurrentPosition(this.getWeather);
         }
         else {
             return geoNotSupported;
@@ -27,7 +52,7 @@ class CurrentWeather extends Component {
     render() {
         return(
             <div>
-                <h2><span class="temp">{this.getWeather()}</span>&deg;</h2>
+                <h2><span class="temp">{this.state.temp.temp}</span>&deg;</h2>
                 <p>more info / graphics here</p>
             </div>
         )
